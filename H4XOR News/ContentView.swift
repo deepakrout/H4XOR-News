23e3ae4b7ba3d2c5c09675fb4e1ec482a2e80227
Deepak <deepakrout@hotmail.com>
2020-06-24 13:33:35 -0400

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Deepak Rout on 6/24/20.
+//  Copyright © 2020 Deepak Rout. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
